722ae765bfc3e5150d3d65fa8eedb9a13300043a
Nathalia <nathalia.cantarelli@edu.pucrs.br>
2023-05-04 23:24:33 -0300

    FIX: Requisito 5: Ajustando nome dos arquivos

--- src/Models/Motorcycle.ODM.ts
+++ src/Models/Motorcycle.ODM.ts
@@ -1,6 +1,6 @@
 import { Schema } from 'mongoose';
 import IMotorcycle from '../Interfaces/IMotorcycle';
-import AbstractODM from './Abstract.ODM';
+import AbstractODM from './AbstractODM';
 
 class MotorcycleODM extends AbstractODM<IMotorcycle> {
   constructor() {
